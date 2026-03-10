8e0e497265b4336fe76b945c4268a75eabc0db54
Arnav Bhasin <bhasin.arnav07@gmail.com>
2026-03-10 05:00:06 -0500

    q3

--- analysis.py
+++ analysis.py
@@ -26,39 +26,38 @@ def question2():
     return answerDiscount, answerNoise
 
 def question3a():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # low discount for close exit, no noise for cliff safety, no living reward to deincentivize living
+    answerDiscount = 0.3
+    answerNoise = 0.0
+    answerLivingReward = 0.0
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
 def question3b():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # low discount for close exit, noise for cliff avoidance, no living reward to deincentivize living
+    answerDiscount = 0.3
+    answerNoise = 0.1
+    answerLivingReward = 0.0
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
 def question3c():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # high discount for far exit, no noise for cliff safety, no living reward to deincentivize living
+    answerDiscount = 0.9
+    answerNoise = 0.0
+    answerLivingReward = 0.0
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
 def question3d():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    # high discount for far exit, noise for cliff avoidance, no living reward to deincentivize living
+    answerDiscount = 0.9
+    answerNoise = 0.1
+    answerLivingReward = 0.0
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
 def question3e():
-    answerDiscount = None
-    answerNoise = None
-    answerLivingReward = None
+    answerDiscount = 0.0
+    answerNoise = 0.0
+    answerLivingReward = 1
     return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
 
 def question8():
     answerEpsilon = None
